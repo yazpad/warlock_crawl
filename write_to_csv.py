78dd752c63c1d4673dfd2834b1208dc30530c3ed
aaronma37 <aam021@eng.ucsd.edu>
2021-07-08 11:16:01 -0700

    update

--- write_to_csv.py
+++ write_to_csv.py
@@ -3,19 +3,29 @@ import csv
 from json import loads
 
 from enums import composition
+from config import study_options
     
-con = sl.connect('isb.db')
+con = sl.connect(study_options['name']+ '.db')
 
 with con:
     csvWriter = csv.writer(open("output.csv", "w"), quoting = csv.QUOTE_NONE,  delimiter='\t', quotechar='', escapechar='|')
     cursor = con.cursor()
-    cursor.execute("SELECT COUNT(*) FROM USER")
+    cursor.execute("SELECT COUNT(*) FROM "  + study_options["name"])
     num = cursor.fetchone()
     print("There are ", num, "entries")
-    data = con.execute("SELECT * FROM USER")
+    data = con.execute("SELECT * FROM "  + study_options["name"])
+    out = " "
     for row in data:
-        out = ""
-        for item in row:
-            out += str(item) + "\t"
-        print(out)
-        csvWriter.writerow([out])
+        out += "\n"
+        for i in range(len(row)):
+            item = row[i]
+            if i == len(row) -1:
+                val = loads(item)
+                print(row)
+                for player_data in val:
+                    for k in sorted(player_data): 
+                        out += str(player_data[k]) + "\t"
+            else:
+                out += str(item) + "\t"
+    print(out)
+    csvWriter.writerow([out])
